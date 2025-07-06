848de9dc134d0deadfb31ec77a6080aca04891c6
thatsbass <bass374@outlook.com>
2025-07-06 22:30:42 +0000

    test

--- src/main/java/com/gosuto/productservice/exceptions/RessourceException.java
+++ src/main/java/com/gosuto/productservice/exceptions/RessourceException.java
@@ -3,5 +3,6 @@ package com.gosuto.productservice.exceptions;
 public class RessourceException extends RuntimeException {
     public RessourceException(String message) {
         super(message);
+        // this message
     }
 }
